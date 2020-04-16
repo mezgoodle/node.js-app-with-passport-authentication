25052b533360b6ac817b19d7c7d6b132f9c32525
mezgoodle <mezgoodle@gmail.com>
2020-04-16 16:02:20 +0300

    Handler for registration

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,11 @@ router.get("/login", (req, res) => res.render("login"));
 // Register page
 router.get("/register", (req, res) => res.render("register"));
 
+// Register Handle
+
+router.post("/register", (req, res) => {
+    console.log(req.body);
+    res.send("hello");
+});
+
 module.exports = router;
