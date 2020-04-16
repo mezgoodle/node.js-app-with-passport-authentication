f72c822c94a1ef56480ff068af59c04ad00c9a22
mezgoodle <mezgoodle@gmail.com>
2020-04-16 19:35:41 +0300

    Validate registration

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,35 @@ router.get("/register", (req, res) => res.render("register"));
 // Register Handle
 
 router.post("/register", (req, res) => {
-    console.log(req.body);
-    res.send("hello");
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    // Check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: "Pleas fill in all fields" });
+    }
+
+    // Check password match
+    if (password !== password2) {
+        errors.push({ msg: "Password do not match" });
+    }
+
+    // Check pass length
+    if (password.length < 6) {
+        errors.push({ msg: "Password should be at least 6 characters" });
+    }
+
+    if (errors.length > 0) {
+        res.render("register", {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        res.send("pass");
+    }
 });
 
 module.exports = router;
