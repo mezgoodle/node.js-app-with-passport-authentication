c3bbb9b9263aba8293e03d625865e416e57102d4
mezgoodle <mezgoodle@gmail.com>
2020-04-17 21:50:52 +0300

    Local strategy for authentication

--- routes/users.js
+++ routes/users.js
@@ -71,7 +71,7 @@ router.post("/register", (req, res) => {
                             newUser.save()
                                 .then(user => {
                                     req.flash("success_msg", "You are now registered and can log in");
-                                    res.redirect("users/login");
+                                    res.redirect("/users/login");
                                 })
                                 .catch(err => console.log(err));
                         }))
