533e44c1044697d3709a2d3c6f367429aeb8db53
mezgoodle <mezgoodle@gmail.com>
2020-04-17 22:18:12 +0300

    Render name

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const { ensureAuthenticated } = require("../config/auth");
 // Welcome page
 router.get("/", (req, res) => res.render("welcome"));
 // Dashboard
-router.get("/dashboard", ensureAuthenticated, (req, res) => res.render("dashboard"));
+router.get("/dashboard", ensureAuthenticated, (req, res) =>
+    res.render("dashboard", {
+        name: req.user.name
+    }));
 
 module.exports = router;
