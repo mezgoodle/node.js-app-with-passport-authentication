3c9920ca5e41e6c7731f010d9e2cc3959dbe3b41
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-04-30 19:43:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -53,4 +53,4 @@ app.use("/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on post ${PORT}`));
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
